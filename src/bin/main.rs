45b155ddf14fc498ae914f14d70fc992bcc7bd5f
Chloe Holmes 3782047 <s3782047@student.rmit.edu.au>
2021-10-25 13:57:07 +1100

    cleaner routing

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -2,12 +2,12 @@ use std::fs;
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
-use std::thread;
-use std::time::Duration;
 use rust_social::ThreadPool;
 use rusqlite::{Connection};
 
+mod router;
 mod user;
+mod httprequest;
 
 fn main() {
     let conn = Connection::open("rust-social.db").unwrap_or(Connection::open_in_memory().unwrap());
@@ -29,6 +29,7 @@ fn main() {
 }
 
 fn generate_tables(conn: &Connection) {
+    conn.execute("drop table users", []);
     conn.execute("create table users (
                 id integer primary key,
                 username text not null unique,
@@ -37,68 +38,26 @@ fn generate_tables(conn: &Connection) {
     );
 }
 
-fn print_type_of<T>(_: &T) {
-    println!("{}", std::any::type_name::<T>())
-}
-
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
 
-    let get = b"GET / HTTP/1.1\r\n";
-    let sleep = b"GET /sleep HTTP/1.1\r\n";
+    let request_obj = httprequest::create_request(buffer);
 
-    let mut request = String::new();
+    let (status_line, body) =
+        match request_obj.uri.as_str() {
+            "/login" => user::login(request_obj, buffer),
+            "/login" => user::register(request_obj, buffer),
+            _ => ("HTTP/1.1 404 NOT FOUND".to_string(), fs::read_to_string("404.html").unwrap_or("404".to_string()))
+        };
 
-    for c in buffer {
-        if c.eq(&b'\r') {
-            break
-        }
-        request.push(char::from(c));
-    }
-    let request: Vec<&str> = request.split(' ').collect();
-    if request.len() == 3 {
-        let request = (request[0], request[1], request[2]);
-    //     valid request, now to send to place
-        let (status_line, body) =
-            if(request.1.eq("/login")) {
-                user::login(request.0, buffer)
-            } else {
-                ("HTTP/1.1 404 NOT FOUND".to_string(), fs::read_to_string("404.html").unwrap())
-            };
+    let response = format!(
+        "{}\r\nContent-Length: {}\r\n\r\n{}",
+        status_line,
+        body.len(),
+        body
+    );
 
-        let response = format!(
-            "{}\r\nContent-Length: {}\r\n\r\n{}",
-            status_line,
-            body.len(),
-            body
-        );
-
-        stream.write(response.as_bytes()).unwrap();
-    } else {
-        let contents = fs::read_to_string("404.html").unwrap();
-
-        let response = format!(
-            "{}\r\nContent-Length: {}\r\n\r\n{}",
-            "HTTP/1.1 404 NOT FOUND",
-            contents.len(),
-            contents
-        );
-
-        stream.write(response.as_bytes()).unwrap();
-    }
-
-    // let (status_line, filename) = if buffer.starts_with(get) {
-    //     ("HTTP/1.1 200 OK", "hello.html")
-    // } else if buffer.starts_with(sleep) {
-    //     thread::sleep(Duration::from_secs(5));
-    //     ("HTTP/1.1 200 OK", "hello.html")
-    // } else if(buffer.starts_with(b"GET /login HTTP/1.1\r\n")) {
-    //     user::login()
-    // } else {
-    //     ("HTTP/1.1 404 NOT FOUND", "404.html")
-    // };
-
-
+    stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
 }
--- src/bin/user.rs
+++ src/bin/user.rs
@@ -1,4 +1,14 @@
-pub fn login(reqType: &str, request: [u8; 1024]) -> (String, String) {
-    ("HTTP/1.1 404 NOT FOUND".to_string(), "hey logged in, or are you".to_string())
+pub struct User {
+    id: i32,
+    username: String,
+    hashedpw: String
+}
+
+pub fn login(request: super::httprequest::Request, buffer: [u8; 1024]) -> (String, String) {
+    ("HTTP/1.1 404 NOT FOUND\r\nSet-Cookie: rat=ratttttt".to_string(), "hey logged in, or are you".to_string())
+//     Login time
+}
+pub fn register(request: super::httprequest::Request, buffer: [u8; 1024]) -> (String, String) {
+    ("HTTP/1.1 404 NOT FOUND\r\nSet-Cookie: rat=ratttttt".to_string(), "register time".to_string())
 //     Login time
 }
