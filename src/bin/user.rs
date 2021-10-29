24be4398415c7a6988f53e36e6a7acb31f18a7e2
Chloe Holmes (s3782047) <s3782047@student.rmit.edu.au>
2021-10-29 12:13:02 +1100

    added login/db functions

--- src/bin/user.rs
+++ src/bin/user.rs
@@ -1,12 +1,59 @@
 use super::httprequest;
-use rusqlite::{Connection, params};
+use rusqlite::{Connection, params, Error};
 use crypto::digest::Digest;
 use crypto::sha3::Sha3;
+use rusqlite::types::{FromSql, FromSqlResult, ValueRef};
+
+#[derive(Debug)]
+pub struct User {
+    pub username: String,
+    pub hashedpw: String
+}
 
 pub fn login(request: httprequest::Request, buffer: [u8; 1024]) -> (String, String) {
-    ("HTTP/1.1 404 NOT FOUND\r\nSet-Cookie: rat=ratttttt".to_string(), "hey logged in, or are you".to_string())
-//     Login time
+
+    if request.req_type == httprequest::TYPE::POST {
+        let parsed = json::parse(request.body.as_str());
+        match parsed {
+            Ok(v) => {
+                if v["username"].is_null() || v["password"].is_null() {
+                    return ("HTTP/1.1 400 BAD REQUEST\r\n".to_string(), "Invalid JSON".to_string());
+                } else {
+                    let conn = Connection::open("rust-social.db").unwrap();
+                    // TODO actually properly do authentication stuff
+
+                    let row: Result<User, Error> = conn.query_row("SELECT * FROM users WHERE username= ?1",
+                                                                  params![v["username"].as_str()], |row| {
+                        Ok(User {
+                            username: row.get(0)?,
+                            hashedpw: row.get(1)?
+                        })
+                    });
+                    // todo check for auth
+                    match row {
+                        Ok(r) => {
+                            return (
+                                "HTTP/1.1 200 OK\r\nSet-Cookie: token=".to_string() +
+                                    to_hash(v["username"].as_str().unwrap(), v["password"].as_str().unwrap()).as_str(),
+                                "Hello ".to_string() + v["username"].as_str().unwrap()
+                            );
+                        }
+                        Err(e) => {
+                            return ("HTTP/1.1 400 BAD REQUEST\r\n".to_string(), e.to_string());
+                        }
+                    }
+                    return ("HTTP/1.1 200 OK\r\n".to_string(), format!("Username - {}\nPassword - {}", row.username, row.hashedpw));
+                }
+            },
+            Err(e) =>
+                return ("HTTP/1.1 400 BAD REQUEST\r\n".to_string(), "Invalid JSON".to_string())
+        }
+    } else {
+        return ("HTTP/1.1 404 NOT FOUND\r\n".to_string(), "pretend there a register page here".to_string());
+    }
 }
+
+
 pub fn register(request: httprequest::Request, buffer: [u8; 1024]) -> (String, String) {
     if request.req_type == httprequest::TYPE::POST {
         let parsed = json::parse(request.body.as_str());
@@ -22,13 +69,9 @@ pub fn register(request: httprequest::Request, buffer: [u8; 1024]) -> (String, S
                         params![v["username"].as_str(), v["password"].as_str()]
                     ) {
                         Ok(_) => {
-                            let mut hasher = Sha3::sha3_512();
-                            hasher.input_str(env!("secret"));
-                            hasher.input_str(v["username"].as_str().unwrap());
-                            hasher.input_str(v["password"].as_str().unwrap());
-
                             return (
-                                "HTTP/1.1 200 OK\r\nSet-Cookie: token=".to_string() + hasher.result_str().as_str(),
+                                "HTTP/1.1 200 OK\r\nSet-Cookie: token=".to_string() +
+                                    to_hash(v["username"].as_str().unwrap(), v["password"].as_str().unwrap()).as_str(),
                                 "Hello ".to_string() + v["username"].as_str().unwrap()
                             );
                         }
@@ -44,5 +87,13 @@ pub fn register(request: httprequest::Request, buffer: [u8; 1024]) -> (String, S
     } else {
         return ("HTTP/1.1 404 NOT FOUND\r\n".to_string(), "pretend there a register page here".to_string());
     }
-//     Login time
+}
+
+fn to_hash(username: &str, password: &str) -> String {
+    let mut hasher = Sha3::sha3_512();
+    hasher.input_str(env!("secret"));
+    hasher.input_str(username);
+    hasher.input_str(password);
+
+    hasher.result_str()
 }
